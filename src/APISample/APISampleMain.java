1251d1a421c9127e2d864fa47521d5560f9b8ac3
zhao0004 <zhao0004@student.monash.edu>
2022-03-31 18:46:50 +0800

    1,initial commit with web service API sample. 2, imported HTTP client library

---
+++ src/APISample/APISampleMain.java
@@ -0,0 +1,230 @@
+package APISample;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class APISampleMain {
+    /*
+    NOTE: In order to access the web service, you will need to include your API key in the Authorization header of all requests you make.
+    Your personal API key can be obtained here: https://fit3077.com
+   */
+    private static final String myApiKey = "hzNMQKz7MBKmgNLG6tQFtBzddLGf6q";
+
+    // Provide the root URL for the web service. All web service request URLs start with this root URL.
+    private static final String rootUrl = "https://fit3077.com/api/v1";
+
+    public static void main(String[] args) throws Exception {
+        // To get a specific resource from the web service, extend the root URL by appending the resource type you are looking for.
+        // For example: [root_url]/user will return a JSON array object containing all users.
+        String usersUrl = rootUrl + "/user";
+
+    /*
+      Part 1 - Unauthenticated requests
+     */
+
+        // This request is unauthenticated (API key not attached), and will return an error.
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest
+                .newBuilder(URI.create(usersUrl))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println("Part 1\n----");
+        System.out.println(request.uri());
+        System.out.println("Response code: " + response.statusCode()); // Status code of 4xx or 5xx indicates an error with the request or with the server, respectively.
+        System.out.println("Full JSON response: " + response.body());
+        System.out.println("----\n\n");
+
+
+    /*
+      Part 2 - Performing GET requests that return an array of objects.
+     */
+
+        // This request is authenticated (API key attached in the Authorization header), and will return the JSON array object containing all users.
+        client = HttpClient.newHttpClient();
+        request = HttpRequest
+                .newBuilder(URI.create(usersUrl))
+                .setHeader("Authorization", myApiKey)
+                .GET()
+                .build();
+
+        response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println("Part 2\n----");
+        System.out.println(request.uri());
+        System.out.println("Response code: " + response.statusCode());
+        System.out.println("Full JSON response: " + response.body());
+
+        System.out.println();
+
+        // Error checking for this sample code. You can check the status code of your request, as part of performing error handling in your assignment.
+        if (response.statusCode() != 200) {
+            throw new Exception("Please specify your API key in line 38 to continue using this sample code.");
+        }
+
+        // The GET /user endpoint returns a JSON array, so we can loop through the response as we could with a normal array/list.
+        ObjectNode[] jsonNodes = new ObjectMapper().readValue(response.body(), ObjectNode[].class);
+
+        for (ObjectNode node: jsonNodes) {
+            System.out.println(node.toString());
+        }
+
+        System.out.println("----\n\n");
+
+        // Get the first user object in the array and keep for future use in later parts of this sample code.
+        ObjectNode userObjectForPart3 = jsonNodes[0];
+
+    /*
+      Part 3a - Performing an invalid GET request to fetch a particular resource by ID.
+     */
+
+        // To get a particular User resource, you will need to specify the user ID you are interested in. Simply extend the users_url by appending the user's id.
+        String usersIdUrl = usersUrl + "/this-is-my-make-believe-id";
+
+        // This request will fail as the user's ID is invalid/does not exist.
+        client = HttpClient.newHttpClient();
+        request = HttpRequest
+                .newBuilder(URI.create(usersIdUrl))
+                .setHeader("Authorization", myApiKey)
+                .GET()
+                .build();
+
+        response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println("Part 3a\n----");
+        System.out.println(request.uri());
+        System.out.println("Response code: " + response.statusCode());
+        System.out.println("Full JSON response: " + response.body());
+        System.out.println("----\n\n");
+
+
+    /*
+      Part 3b - Performing a valid GET request to fetch a particular resource by ID
+     */
+
+        // This request will succeed (assuming there was at least one user object returned in the array from Part 2.
+        usersIdUrl = usersUrl + "/" + userObjectForPart3.get("id").textValue();
+        client = HttpClient.newHttpClient();
+        request = HttpRequest
+                .newBuilder(URI.create(usersIdUrl))
+                .setHeader("Authorization", myApiKey)
+                .GET()
+                .build();
+
+        response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println("Part 3b\n----");
+        System.out.println(request.uri());
+        System.out.println("Response code: " + response.statusCode());
+        System.out.println("Full JSON response: " + response.body());
+        System.out.println("----\n\n");
+
+
+    /*
+      Part 3c - Performing a valid GET request to fetch a particular resource by ID, with query parameters.
+     */
+
+        // To get more data for users (e.g. their bookings and COVID tests taken), you can add query params to the request.
+        // You can directly append the query string to the end of the request URL - e.g. usersIdUrl + "?fields=bookings"
+        // To get multiple fields, you can use: usersIdUrl + "?fields=bookings&fields=testsTaken"
+        client = HttpClient.newHttpClient();
+        request = HttpRequest
+                .newBuilder(URI.create(usersIdUrl + "?fields=bookings"))
+                .setHeader("Authorization", myApiKey)
+                .GET()
+                .build();
+
+        response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println("Part 3c\n----");
+        System.out.println(request.uri());
+        System.out.println("Response code: " + response.statusCode());
+        System.out.println("Full JSON response: " + response.body()); // Note that the object now has the bookings property returned.
+        System.out.println("----\n\n");
+
+    /*
+      Part 4 - Authenticating a user's credentials using the POST /user/login endpoint
+     */
+
+        // Create the request body. The password for each of the sample user objects that have been created for you are the same as their respective usernames.
+        String jsonString = "{" +
+                "\"userName\":\"" + userObjectForPart3.get("userName").textValue() + "\"," +
+                "\"password\":\"" + userObjectForPart3.get("userName").textValue() + "\"" +
+                "}";
+
+        // Note the POST() method being used here, and the request body is supplied to it.
+        // A request body needs to be supplied to this endpoint, otherwise a 400 Bad Request error will be returned.
+        String usersLoginUrl = usersUrl + "/login";
+        client = HttpClient.newHttpClient();
+        request = HttpRequest.newBuilder(URI.create(usersLoginUrl + "?jwt=true")) // Return a JWT so we can use it in Part 5 later.
+                .setHeader("Authorization", myApiKey)
+                .header("Content-Type","application/json") // This header needs to be set when sending a JSON request body.
+                .POST(HttpRequest.BodyPublishers.ofString(jsonString))
+                .build();
+
+        response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println("Part 4\n----");
+        System.out.println(request.uri());
+        System.out.println("Response code: " + response.statusCode());
+        System.out.println("Full JSON response: " + response.body()); // The JWT token that has just been issued will be returned since we set ?jwt=true.
+        System.out.println("----\n\n");
+
+
+    /*
+      Part 5a - Verifying a JWT using the POST /user/verify-token endpoint
+     */
+        ObjectNode jsonNode = new ObjectMapper().readValue(response.body(), ObjectNode.class);
+
+        jsonString = "{\"jwt\":\"" + jsonNode.get("jwt").textValue() + "\"}";
+
+        // Note the POST() method being used here, and the request body is supplied to it.
+        // A request body needs to be supplied to this endpoint, otherwise a 400 Bad Request error will be returned.
+        String usersVerifyTokenUrl = usersUrl + "/verify-token";
+        client = HttpClient.newHttpClient();
+        request = HttpRequest.newBuilder(URI.create(usersVerifyTokenUrl)) // Return a JWT so we can use it in Part 5 later.
+                .setHeader("Authorization", myApiKey)
+                .header("Content-Type","application/json") // This header needs to be set when sending a JSON request body.
+                .POST(HttpRequest.BodyPublishers.ofString(jsonString))
+                .build();
+
+        response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println("Part 5a\n----");
+        System.out.println(request.uri());
+        System.out.println("Response code: " + response.statusCode());
+        System.out.println("Full JSON response: " + response.body()); // This endpoint does not return a JSON response upon success.
+        System.out.println("----\n\n");
+
+
+    /*
+      Part 5b - Verifying a forged/tampered JWT using the POST /user/verify-token endpoint
+     */
+        jsonString = "{\"jwt\":\"" + jsonNode.get("jwt").textValue() + "tampered" + "\"}"; // Tamper with the JWT.
+
+        // Note the POST() method being used here, and the request body is supplied to it.
+        // A request body needs to be supplied to this endpoint, otherwise a 400 Bad Request error will be returned.
+        // A JSON response will only be returned by the POST /user/verify-token endpoint if there were problems verifying the JWT.
+        client = HttpClient.newHttpClient();
+        request = HttpRequest.newBuilder(URI.create(usersVerifyTokenUrl))
+                .setHeader("Authorization", myApiKey)
+                .header("Content-Type","application/json") // This header needs to be set when sending a JSON request body.
+                .POST(HttpRequest.BodyPublishers.ofString(jsonString))
+                .build();
+
+        response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println("Part 5b\n----");
+        System.out.println(request.uri());
+        System.out.println("Response code: " + response.statusCode());
+        System.out.println("Full JSON response: " + response.body()); // Note that all 4xx and 5xx errors are guaranteed to return a JSON response.
+        System.out.println("----\n\n");
+    }
+}
