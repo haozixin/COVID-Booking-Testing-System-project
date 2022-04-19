f4a1c8bade714d027386f47136c669cd18be0c67
zhao0004 <zhao0004@student.monash.edu>
2022-04-19 18:23:33 +0800

    dummy test commit

--- src/Application.java
+++ src/Application.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-
 public class Application {
     public static void main(String[] args) throws IOException, InterruptedException {
 
