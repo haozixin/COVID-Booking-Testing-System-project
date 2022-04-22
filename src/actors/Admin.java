9b009b8fd34fdf11241b52a8c1971e9b7415e27d
tyan0027 <tyan0027@student.monash.edu>
2022-04-22 21:54:18 +1000

    actors updated

--- src/actors/Admin.java
+++ src/actors/Admin.java
@@ -5,8 +5,6 @@ import engine.actions.Action;
 import engine.actions.Actions;
 
 public class Admin extends Actor {
-
-
     @Override
     public Action playTurn(Actions actions, Action lastAction, Display display) {
         return null;
--- src/actors/HealthcareWorker.java
+++ src/actors/HealthcareWorker.java
@@ -5,8 +5,6 @@ import engine.actions.Action;
 import engine.actions.Actions;
 
 public class HealthcareWorker extends Actor {
-
-
     @Override
     public Action playTurn(Actions actions, Action lastAction, Display display) {
         return null;
