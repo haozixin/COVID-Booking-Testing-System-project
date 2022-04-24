861fac3d56dac7b8851fe62bc4fe0957cf898712
tyan0027 <tyan0027@student.monash.edu>
2022-04-24 17:00:53 +1000

    Interview function logic achieved

--- src/actions/InterviewAction.java
+++ src/actions/InterviewAction.java
@@ -20,7 +20,6 @@ public class InterviewAction extends Action {
     private ArrayList<String> symptomFields;
 
     public InterviewAction() {
-        
         name = "Interview Action";
 
         symptomFields = new ArrayList<>();
@@ -49,44 +48,7 @@ public class InterviewAction extends Action {
             }
         }
 
-
-//        System.out.println("Does the customer have a flu? (y/n): ");
-//        answer = s.nextLine().trim();
-//        if (answer == "y") {
-//            no_of_yes += 1;
-//        }
-//
 //        //TODO: 做一个interview的流程， 问几个问题， 根据答案来推荐做哪种测试， 然后再问医师的决定，记录并上传”决定“
-//        System.out.println("Does the customer have a cough? (y/n): ");
-//        answer = s.nextLine().trim();
-//        if (answer == "y") {
-//            no_of_yes += 1;
-//        }
-//
-//        System.out.println("Does the customer have a fever? (y/n): ");
-//        answer = s.nextLine().trim();
-//        if (answer == "y") {
-//            no_of_yes += 1;
-//        }
-//
-//        System.out.println("Does the customer have a headache? (y/n): ");
-//        answer = s.nextLine().trim();
-//        if (answer == "y") {
-//            no_of_yes += 1;
-//        }
-//
-//        System.out.println("Does the customer have a chest pain? (y/n): ");
-//        answer = s.nextLine().trim();
-//        if (answer == "y") {
-//            no_of_yes += 1;
-//        }
-//
-//        System.out.println("Does the customer have a diarrhoea? (y/n): ");
-//        answer = s.nextLine().trim();
-//        if (answer == "y") {
-//            no_of_yes += 1;
-//        }
-//
         return "success";
     }
 
